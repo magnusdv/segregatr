6847f1f886593676399144173aed8ebe7022799f
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-09-11 10:40:01 +0200

    Factor out pedigree plot in separate function

---
+++ R/plotPedDetails.R
@@ -0,0 +1,40 @@
+#' @importFrom graphics arrows strheight text
+plotPedDetails = function(x, affected = NULL, unknown = NULL, proband = NULL,
+                          carriers = NULL, noncarriers = NULL, cex = 1,
+                          margins = rep(1, 4), ...) {
+
+  # Input checks
+  allids = c(proband, affected, unknown, carriers, noncarriers)
+  if(any(!allids %in% labels(x)))
+    stop2("Unknown ID label: ", setdiff(allids, labels(x)))
+
+  if(length(err1 <- intersect(affected, unknown)))
+    stop2("Individual specified as both affected and unknown: ", err1)
+
+  if(length(err2 <- intersect(carriers, noncarriers)))
+    stop2("Individual specified as both a carrier and a non-carrier: ", err2)
+
+  p = plot(x,
+           aff = affected,
+           textInside = ifelse(1:pedsize(x) %in% unknown, "?", ""),
+           cex = cex,
+           margins = margins,
+           keep.par = TRUE,
+           ...)
+
+  vdist = p$boxh + 3.25 * abs(strheight("M", cex = cex))  # vertical dist from top of symbol to "+"
+
+  if(!is.null(carriers))
+    text(p$x[carriers], p$y[carriers] + vdist, labels = "+", cex = cex*1.5, font = 1, pos = 3, offset = 0)
+
+  if(!is.null(noncarriers))
+    text(p$x[noncarriers], p$y[noncarriers] + vdist, labels = "-", cex = cex*1.5, font = 1, pos = 3, offset = 0)
+
+  # proband arrow
+  if(!is.null(proband)) {
+    prob = internalID(x, proband)
+    corner.x = p$x[prob] - .5*p$boxw
+    corner.y = p$y[prob] + p$boxh
+    arrows(corner.x - 1.7*p$boxw, corner.y + 0.9*p$boxh, corner.x - .5*p$boxw, corner.y , lwd = 2, length = .15, xpd = NA)
+  }
+}
