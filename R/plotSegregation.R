32fe2e4a1ce02855b6ae35dc610fd0fd62f4d22e
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-12-12 12:23:08 +0100

    More input checks in `plotSegregation()`
    * Add unit tests

--- R/plotSegregation.R
+++ R/plotSegregation.R
@@ -22,7 +22,7 @@ plotSegregation = function(x, affected = NULL, unknown = NULL, proband = NULL,
                            margins = 1, ...) {
 
   # Input checks
-  allids = c(proband, affected, unknown, carriers, noncarriers)
+  allids = c(proband, affected, unknown, carriers, noncarriers, homozygous)
   if(any(!allids %in% labels(x)))
     stop2("Unknown ID label: ", setdiff(allids, labels(x)))
 
@@ -32,14 +32,22 @@ plotSegregation = function(x, affected = NULL, unknown = NULL, proband = NULL,
   if(length(err2 <- intersect(carriers, noncarriers)))
     stop2("Individual specified as both a carrier and a non-carrier: ", err2)
 
-  if(length(proband) > 1)
+  if(length(err3 <- intersect(carriers, homozygous)))
+    stop2("Individual specified as both a (heterozygous) carrier and homozygous: ", err3)
+
+  if(length(err4 <- intersect(noncarriers, homozygous)))
+    stop2("Individual specified as both a non-carrier and homozygous: ", err4)
+
+  hasProband = length(proband) > 0
+
+  if(hasProband && length(proband) > 1)
     stop2("At most one proband is permitted")
 
   # Invoke automatic margin adjustment unless fully specified
   autoMargins = length(margins) < 4
 
   # Adjust margins if arrow on left side
-  if(autoMargins && packageVersion("pedtools") > 2.0) {
+  if(hasProband && autoMargins && packageVersion("pedtools") > 2.0) {
 
     align = .pedAlignment(x, ...)
     idx = match(internalID(x, proband), align$plotord)
@@ -73,7 +81,7 @@ plotSegregation = function(x, affected = NULL, unknown = NULL, proband = NULL,
     text(p$x[noncarriers], p$y[noncarriers] + vdist, labels = "-", cex = cex*1.5, font = 1, pos = 3, offset = 0)
 
   # proband arrow
-  if(!is.null(proband)) {
+  if(hasProband) {
     corner.x = p$x[proband] - .5*p$boxw
     corner.y = p$y[proband] + p$boxh
     arrows(corner.x - 1.7*p$boxw, corner.y + 0.9*p$boxh, corner.x - .5*p$boxw, corner.y ,
---
+++ tests/testthat/test-segregation-plot.R
@@ -0,0 +1,25 @@
+test_that("plotSegregation() catches errors", {
+
+  x = nuclearPed()
+
+  expect_silent(plotSegregation(x))
+  expect_error(plotSegregation(x, proband = 1:2), "At most one proband is permitted")
+  expect_error(plotSegregation(x, proband = 4), "Unknown ID label:")
+
+  expect_error(plotSegregation(x, affected = 4), "Unknown ID label:")
+  expect_error(plotSegregation(x, unknown = 4), "Unknown ID label:")
+  expect_error(plotSegregation(x, affected = 1, unknown = 1),
+               "Individual specified as both affected and unknown")
+
+
+  expect_error(plotSegregation(x, carriers = 4), "Unknown ID label:")
+  expect_error(plotSegregation(x, noncarriers = 4), "Unknown ID label:")
+  expect_error(plotSegregation(x, carriers = 1, noncarriers = 1),
+               "Individual specified as both a carrier and a non-carrier")
+
+  expect_error(plotSegregation(x, homozygous = 4), "Unknown ID label:")
+  expect_error(plotSegregation(x, carriers = 1, homozygous = 1),
+               "Individual specified as both a (heterozygous) carrier and homozygous", fixed = TRUE)
+  expect_error(plotSegregation(x, noncarriers = 1, homozygous = 1),
+               "Individual specified as both a non-carrier and homozygous")
+})
